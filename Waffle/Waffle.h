b174c21e955b3d9baca0326827f395ee44ec82fb
Jakob Jensen <jje@trifork.com>
2017-05-25 19:16:35 +0200

    Become carthage compatible.

---
+++ Waffle/Waffle.h
@@ -0,0 +1,19 @@
+//
+//  Waffle.h
+//  Waffle
+//
+//  Created by Jakob Jensen on 25/05/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Waffle.
+FOUNDATION_EXPORT double WaffleVersionNumber;
+
+//! Project version string for Waffle.
+FOUNDATION_EXPORT const unsigned char WaffleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Waffle/PublicHeader.h>
+
+
